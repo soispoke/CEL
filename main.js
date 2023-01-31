7beb806b8ca979b94b06e2f09485cf817c79db68
soispoke <66172107+soispoke@users.noreply.github.com>
2023-01-31 08:39:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ alert("This alert box was called with the onload event");
 var pyshell = new PythonShell('main.py');
 
 const blockchain = "bnb";
-const address = "0x0d8ce2a99bb6e3b7db580ed848240e4a0f9ae153";
-const start_date = "2022-09-24 00:00:00";
-const end_date = "2022-10-01 00:00:00";
+const address = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+const start_date = "2023-01-23 00:00:00";
+const end_date = "2023-01-27 00:00:00";
 
 pyshell.send(JSON.stringify([
     blockchain,
